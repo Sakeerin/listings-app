433eee56e7b1a212ee7e65f4959b390195dbf517
Sakeerin <theblue0800@gmail.com>
2025-05-12 10:51:35 +0700

    add function login

---
+++ app/Http/Controllers/Auth/AuthenticateController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class AuthenticateController extends Controller
+{
+    public function create()
+    {
+        return Inertia::render('Auth/Login');
+    }
+
+    public function store(Request $request)
+    {
+        // sleep(2);
+        $credentials = $request->validate([
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:3'
+        ]);
+
+        if (Auth::attempt($credentials, $request->boolean('remember'))) {
+            $request->session()->regenerate();
+
+            return redirect()->intended();
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.'
+        ])->onlyInput('email');
+
+    }
+}
