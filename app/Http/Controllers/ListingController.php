dde3eb245d00093b9cce5a2211d0b6b34d5b31cc
Sakeerin Khami <sakeerin.k@vbeyond.co.th>
2025-05-25 09:58:03 +0700

    add function search filter

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreListingRequest;
 use App\Http\Requests\UpdateListingRequest;
+use Illuminate\Http\Request;
 use App\Models\Listing;
 use Inertia\Inertia;
 
@@ -12,11 +13,17 @@ class ListingController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $listings = Listing::with('user')->latest()->paginate(6);
-        return Inertia::render('Home',[
+        $listings = Listing::with('user')
+            ->filter(request(['search']))
+            ->latest()
+            ->paginate(6)
+            ->withQueryString();
+
+        return Inertia::render('Home', [
             'listings' => $listings,
+            'searchTerm' => $request->search,
         ]);
     }
 
